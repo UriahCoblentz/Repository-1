ebfc70d717f19d2aaec000a9e3b7b1428252836c
UriahCoblentz <46757461+UriahCoblentz@users.noreply.github.com>
2019-01-16 12:27:51 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+/***************************************************************************
+ * First Project
+ ***************************************************************************
+ * Printing Student Information 
+ *__________________________________________________________________________
+ * Uriah Coblentz
+ * 1/16/2019
+ * CMSC 255 004
+ ***************************************************************************/
+ 
+ 
+ public class Hello {
+  public static void printHeading() {
+   System.out.println("Uriah Coblentz");
+   System.out.println("First Project");
+   System.out.println("Printing Student Information");
+   System.out.println("1/16/2019");
+   System.out.println("CMSC 255 004");
+   System.out.println();
+  }
+  
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
+   
